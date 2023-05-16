81cae2380622de30977a639a5d098861612de128
ShahzodTuraev <turayevshahzodbek@gmail.com>
2023-05-16 11:35:16 +0900

    buttons created

--- src/table/index.jsx
+++ src/table/index.jsx
@@ -1,26 +1,118 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import axios from 'axios'
-import { Body, BodyText, BodyWrap, Container, Head, HeadText, Main } from './style'
+import { Body, BodyText, BodyWrap, Btn, BtnBox, Container, Head, HeadText, LoginBox, LoginBtn, LoginInput, Main } from './style'
 const TablePage = () => {
-  const[users, setUsers] = useState([])
+  const [users, setUsers] = useState([])
+  const [emailList, setEmailList] = useState([])
+  const [smsList, setSmsList] = useState([])
+  const [memberList, setMemberList] = useState([])
+// PAGES' DISPLAY STATES : 
+  const [access, setAccess] = useState(false)
+  const [userPage, setUserPage] = useState(true)
+  const [emailPage, setEmailPage] = useState(false)
+  const [smsPage, setSmsPage] = useState(false)
+  const [memberPage, setMemberPage] = useState(false)
+  const [id, setId] = useState('')
+  const [password, setPassword] = useState('')
+  // MAIN PAYMENT USERS INFO DATA :
   useEffect(()=>{
-    axios.post('https://api.mever.me:8080/paymentList', {
+    axios.post(`https://api.mever.me:8080/paymentList?email=test@naver.com`, {
     }).then((data)=>{
       setUsers(data.data)
     });
+  }, [])
+  console.log(users);
+  // EMAIL SENDING CONTENT DATA :
+  useEffect(()=>{
+    axios.post('https://api.mever.me:8080/send/list?type=mail', {
+    }).then((data)=>{
+      setEmailList(data.data)
+    });
+  }, [])
+  // SMS SENDING CONTENT DATA :
+  useEffect(()=>{
+    axios.post('https://api.mever.me:8080/send/list?type=sms', {
+    }).then((data)=>{
+      setSmsList(data.data)
+    });
+  }, [])
+  // MEMBER DATA :
+  useEffect(()=>{
+    axios.post('https://api.mever.me:8080/member/list', {
+    }).then((data)=>{
+      setMemberList(data.data)
+    });
+  }, [])
 
-    // console.log(users);
-    
-    // fetch('https://api.mever.me:8080/paymentList')
-    //   .then(response => response.json())
-    //   .then((data)=>{
-    //     console.log(data);
-    //   })
-  }, [users])
+  // LOGIN PAGE FUNCTIONS:
+  const onId = (e) => {
+    setId(e.target.value)
+  }
+  const onPassword = (e) => {
+    setPassword(e.target.value);
+  }
+
+  const onSubmit = () => {
+    if(id === 'ceo' && password === '1111')setAccess(true)
+  }
+
+
+// PAGE BUTTON CONTROL:
+const onPayment = () => {
+  setUserPage(true)
+  setEmailPage(false)
+  setSmsPage(false)
+  setMemberPage(false)
+};
+const onEmail = () => {
+  setUserPage(false)
+  setEmailPage(true)
+  setSmsPage(false)
+  setMemberPage(false)
+};
+const onSms = () => {
+  setUserPage(false)
+  setEmailPage(false)
+  setSmsPage(true)
+  setMemberPage(false)
+
+};
+const onMember = () => {
+  setUserPage(false)
+  setEmailPage(false)
+  setSmsPage(false)
+  setMemberPage(true)
+};
+
+// SEARCHING FUNCTION: 
+const dateRef = useRef()
+const nameRef = useRef()
+const emailRef = useRef()
+const phoneRef = useRef()
+const survayRef = useRef()
+const productRef = useRef()
+const priceRef = useRef()
+
   return (
     <Container>
+      <LoginBox style={access ? {display: 'none'} : {display: 'flex'}}>
+        <h3>아이디와 비밀번호를 입력해주세요</h3>
+        <LoginInput type='text' onChange={onId} placeholder='아이디'/>
+        <LoginInput type='password' onChange={onPassword} placeholder='비밀번호'/>
+        <LoginBtn onClick={onSubmit}>DB 보기</LoginBtn>
+      </LoginBox>
+      {access && 
+        <BtnBox>
+          <Btn onClick={onPayment} style={userPage ? {color:'#000', background: 'coral'} : {color: '#fff'}}>결제 내역</Btn>
+          <Btn onClick={onEmail} style={emailPage ? {color:'#000', background: 'coral'} : {color: '#fff'}}>이메일</Btn>
+          <Btn onClick={onSms} style={smsPage ? {color:'#000', background: 'coral'} : {color: '#fff'}}>메시지</Btn>
+          <Btn onClick={onMember} style={memberPage ? {color:'#000', background: 'coral'} : {color: '#fff'}}>고객</Btn>
+        </BtnBox>
+      }
+      
+      {access && userPage &&
+      
       <Main>
-        {users.length}
         <Head>
           <HeadText>일자</HeadText>
           <HeadText>이름</HeadText>
@@ -28,22 +120,98 @@ const TablePage = () => {
           <HeadText>전화번호</HeadText>
           <HeadText>설문 조사 결과</HeadText>
           <HeadText>상품명</HeadText>
+          <HeadText>상품 가격</HeadText>
+        </Head>
+        <Head>
+          <input ref={dateRef} type="text" />
+          <input ref={nameRef} type="text" />
+          <input ref={emailRef} type="text" />
+          <input ref={phoneRef} type="text" />
+          <input ref={survayRef} type="text" />
+          <input ref={productRef} type="text" />
+          <input ref={priceRef} type="text" />
         </Head>
         <BodyWrap>
-          {users.map((user)=>(
-            <Body key={user.id}>
-              <BodyText>{user.regdate}</BodyText>
+          {users.map((user, index)=>(
+            <Body key={index} style={index % 2 === 0 ? {background: 'rgba(0, 0, 0, 0.05)'} : {background: 'white'}}>
+              <BodyText>{user.approvedAt}</BodyText>
               <BodyText>{user.name}</BodyText>
               <BodyText>{user.email}</BodyText>
               <BodyText>{user.phone}</BodyText>
               <BodyText>{user.dcrp}</BodyText>
               <BodyText>{user.orderName}</BodyText>
+              <BodyText>{`${user.totalAmount?user.totalAmount:0}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')} 원</BodyText>
             </Body>
           ))}
         </BodyWrap>
-        
-
-      </Main>
+      </Main>}
+      {/* EMAIL LIST */}
+      {access && emailPage &&
+      <Main>
+        <Head>
+          <HeadText>일자</HeadText>
+          <HeadText>이메일</HeadText>
+          <HeadText>제목</HeadText>
+          <HeadText>내용</HeadText>
+          <HeadText>전화번호</HeadText>
+        </Head>
+        <BodyWrap>
+          {emailList.map((list, index)=>(
+            <Body key={index} style={index % 2 === 0 ? {background: 'rgba(0, 0, 0, 0.05)'} : {background: 'white'}}>
+              <BodyText>{list.date}</BodyText>
+              <BodyText>{list.email}</BodyText>
+              <BodyText>{list.title}</BodyText>
+              <BodyText>{list.content}</BodyText>
+              <BodyText>{list.phone}</BodyText>
+            </Body>
+          ))}
+        </BodyWrap>
+      </Main>}
+      {/* SMS LIST */}
+      {access && smsPage &&
+      <Main>
+        <Head>
+          <HeadText>일자</HeadText>
+          <HeadText>이메일</HeadText>
+          <HeadText>내용</HeadText>
+          <HeadText>전화번호</HeadText>
+        </Head>
+        <BodyWrap>
+          {smsList.map((list, index)=>(
+            <Body key={index} style={index % 2 === 0 ? {background: 'rgba(0, 0, 0, 0.05)'} : {background: 'white'}}>
+              <BodyText>{list.date}</BodyText>
+              <BodyText>{list.email}</BodyText>
+              <BodyText>{list.content}</BodyText>
+              <BodyText>{list.phone}</BodyText>
+            </Body>
+          ))}
+        </BodyWrap>
+      </Main>}
+       {/* MEMBER LIST */}
+      {access && memberPage &&
+      <Main>
+        <Head>
+          <HeadText>일자</HeadText>
+          <HeadText>이름</HeadText>
+          <HeadText>이메일</HeadText>
+          <HeadText>전화번호</HeadText>
+          <HeadText>설문 조사 결과</HeadText>
+          <HeadText>내용</HeadText>
+        </Head>
+        <BodyWrap>
+          {memberList.map((list, index)=>(
+            <Body key={index} style={index % 2 === 0 ? {background: 'rgba(0, 0, 0, 0.05)'} : {background: 'white'}}>
+              <BodyText>2023-05-15 13:45</BodyText>
+              <BodyText>{list.name}</BodyText>
+              <BodyText>{list.email}</BodyText>
+              <BodyText>{list.phone}</BodyText>
+              <BodyText>{list.dcrp}</BodyText>
+              <BodyText>{list.survay}</BodyText>
+            </Body>
+          ))}
+        </BodyWrap>
+      </Main>}
+      
     </Container>
   )
 }
--- src/table/style.js
+++ src/table/style.js
@@ -6,14 +6,19 @@ const Container = styled.div`
   align-items: center;
   padding: 30px;
 `;
-const Main = styled.table`
+const Main = styled.div`
   max-width: 70vw;
   width: 100%;
-  height: 100px;
+  height: 95vh;
+  overflow-y: scroll;
   border: 1px solid coral;
+  border-radius: 10px;
 `
 const Head = styled.div`
 display: flex;
+position: sticky;
+top: 0px;
+width: 100%;
 justify-content: space-around;
 background-color: coral;
 `
@@ -27,6 +32,7 @@ const Body = styled.div`
 display: flex;
 justify-content: space-around;
 
+
 &:hover{
   background: rgba(0, 0, 0, 0.1);
 }
@@ -39,5 +45,61 @@ text-align: center;
 const BodyWrap = styled.div`
 display: flex;
 flex-direction: column-reverse;
-`
-export {Container, Main, Head, HeadText, Body, BodyText, BodyWrap}
+`;
+const LoginBox = styled.div`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform:translate(-50%, -50%);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px;
+  width: 400px;
+  height: 300px;
+  color: #fff;
+  background: #0E3B68;
+  border-radius: 10px;
+`;
+const LoginInput = styled.input`
+  height: 40px;
+  width: 260px;
+  border-radius: 8px;
+  margin-bottom: 20px;
+  outline: 0;
+  padding:0 20px;
+  font-size: 1rem;
+`;
+const LoginBtn = styled.div`
+  height: 40px;
+  width: 300px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 8px;
+  font-size: 1.2rem;
+  font-weight: 600;
+  background: #1CBCFF;
+  cursor: pointer;
+`;
+const BtnBox = styled.div`
+  position: fixed;
+  top: 2rem;
+  left: 2rem;
+`;
+const Btn = styled.div`
+  width: 200px;
+  height: 40px;
+  font-size: 1.2rem;
+  font-weight: 550;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
+  border-radius: 8px;
+  margin: 10px 0;
+  background: #1CBCFF;
+  color: #fff;
+  cursor: pointer;
+`;
+export {Container, Main, Head, HeadText, Body, BodyText, BodyWrap, LoginBox, LoginBtn, LoginInput, BtnBox, Btn}
